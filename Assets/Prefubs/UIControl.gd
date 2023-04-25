f827525c6b9c50c73b7ea2713b1d7a056fa7dc75
kortm <cort202@gmail.com>
2023-04-26 01:41:29 +0300

    create build panel

--- Assets/Prefubs/UIControl.gd
+++ Assets/Prefubs/UIControl.gd
@@ -7,6 +7,10 @@ onready var help_button = get_node("settings/Panel/VBoxContainer/help_button")
 onready var save_and_exit = get_node("settings/Panel/VBoxContainer/save_and_exit")
 onready var cancel_button = get_node("settings/subpanel/NinePatchRect/HBoxContainer/cancel_button")
 onready var apply_button = get_node("settings/subpanel/NinePatchRect/HBoxContainer/apply_button")
+onready var build_panel = get_node("NinePatchRect/HBoxContainer/Panel/build_panel")
+onready var one_build_panel = get_node("NinePatchRect/HBoxContainer/Panel/build_panel/TextureButton")
+onready var build_button_back = get_node("NinePatchRect/HBoxContainer/Panel/build_panel/button_back")
+var build_buttons = []
 
 func _ready():
 	menu_button.connect("pressed", self, "_show_panel")
@@ -15,6 +19,10 @@ func _ready():
 	cancel_button.connect("pressed", self, "_close_settings_panel")
 	apply_button.connect("pressed", self, "_apply_config")
 	save_and_exit.connect("pressed", self, "_save_and_exit")
+	one_build_panel.connect("pressed", self, "one_build_button")
+	build_button_back.connect("pressed", self, "build_button_back")
+	for i in range(build_panel.get_child_count()):
+		build_buttons.append(build_panel.get_child(i))
 
 func _show_panel():
 	$settings/Panel.visible = true
@@ -34,3 +42,20 @@ func _apply_config():
 func _save_and_exit():
 	print("Сохраняю и выхожу")
 	get_tree().change_scene("res://Assets/Scenes/Start.tscn")
+
+func one_build_button():
+	#удаляем все кнопки из панели строительства
+	#добавляем кнопку назад и кнопку здания
+	for i in range(build_panel.get_child_count()):
+		build_panel.remove_child(build_panel.get_child(0))
+	print("Drop all element")
+	build_panel.add_child(build_button_back)
+	build_panel.add_child(one_build_panel)
+	
+func build_button_back():
+	#Возращаем все кнопки кроме кнопок back и cancel
+	print("Back")
+	build_panel.remove_child(build_panel.get_child(0))
+	for i in build_buttons:
+		if i.name != "button_back" and i.name != "button_cancel":
+			build_panel.add_child(i)
